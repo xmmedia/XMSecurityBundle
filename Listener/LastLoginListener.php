6b798989916daf5dbf6322a14e74a870787b8ecd
Darryl Hein <dhein@xmmedia.com>
2017-02-26 21:30:44 -0700

    ensuring password set is considered a last login

---
+++ Listener/LastLoginListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace XM\SecurityBundle\Listener;
+
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LastLoginListener implements EventSubscriberInterface
+{
+    /**
+     * @var UserManagerInterface
+     */
+    protected $userManager;
+
+    /**
+     * LastLoginListener constructor.
+     *
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        $events = parent::getSubscribedEvents();
+
+        $events[FOSUserEvents::RESETTING_RESET_COMPLETED] = 'onReset';
+
+        return $events;
+    }
+
+    /**
+     * @param FilterUserResponseEvent $event
+     */
+    public function onReset(FilterUserResponseEvent $event)
+    {
+        $user = $event->getUser();
+
+        $user->setLastLogin(new \DateTime());
+        $this->userManager->updateUser($user);
+    }
+}
